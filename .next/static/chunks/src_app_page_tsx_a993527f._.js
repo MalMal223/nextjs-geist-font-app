2168266bccb765fee3ab7e76433953fc407c8ccb
MalMal223 <157064839+MalMal223@users.noreply.github.com>
2025-06-28 04:17:17 +0800

    Add .next/static/chunks/src_app_page_tsx_a993527f._.js 1751055434118

---
+++ .next/static/chunks/src_app_page_tsx_a993527f._.js
@@ -0,0 +1,10 @@
+(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
+    script: typeof document === "object" ? document.currentScript : undefined,
+    chunks: [
+  "static/chunks/node_modules_tailwind-merge_dist_bundle-mjs_mjs_b854acb4._.js",
+  "static/chunks/node_modules_docx_dist_index_mjs_5380db45._.js",
+  "static/chunks/node_modules_a2c1c721._.js",
+  "static/chunks/src_f1c84b2f._.js"
+],
+    source: "dynamic"
+});
